ccffaf85b4c594eda80f406ae6790f6f556555be
artur <arturgonch20032018@mail.ru>
2022-12-31 15:05:44 +0300

    main function

--- programmer.cpp
+++ programmer.cpp
@@ -1,4 +1,25 @@
 #include "programmer.h"
+std::map<std::string, int (*)(int ,int)> code{{"AND",AND},{"OR",OR},{"XOR",XOR}};
 void programmer(){
+    std::cout << "chose the operation\n";
+    std::string s;
+    std::cin >> s;
+    if (code.count(s)) {
+        std::cout << "enter number\n";
+        int a,b;
+        std::cin >> a >> b;
+        std::cout << code[s](a,b)<<"\n";
+        return;
+    }
+    std::cout << "Operation is not found\n";
+}
 
+int AND (int a,int b){
+    return a&=b;
+}
+int OR (int a,int b){
+    return a|=b;
+}
+int XOR(int a,int b){
+    return a^=b;
 }
--- programmer.h
+++ programmer.h
@@ -4,5 +4,12 @@
 
 #ifndef CALCULATOR_PROGRAMMER_H
 #define CALCULATOR_PROGRAMMER_H
+#include "iostream"
+#include "map"
 void programmer();
+int AND (int a,int b);
+int OR (int a,int b);
+int XOR(int a,int b);
+
+
 #endif //CALCULATOR_PROGRAMMER_H
